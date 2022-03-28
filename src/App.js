5a3114a694cb95db2eb567cc917b4173f2e125d9
bosen L <bosenli2013@hotmail.com>
2022-03-27 23:59:18 -0400

    lifting state react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+
+//component list the fruit, it will need an array of fruits to display
+//an input to capture the filter value from the user. This will need one piece of data, the current value of the filter
+
+//State
+// The filtered 
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
